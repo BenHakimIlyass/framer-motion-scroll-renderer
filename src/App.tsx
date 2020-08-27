b7b7ea9116fc9bae52ff89c1039f180d5fa274fa
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2020-08-27 15:54:23 +0100

    Styling texts

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,28 @@ import "./styles.css";
 import { makeProps, makeMin, makeMax } from "./helpers";
 
 export default function App() {
-  const { isOnScreen } = useScroll({ min: makeMin(3), max: makeMax(5) });
+  const { isOnScreen } = useScroll({ min: makeMin(2.5), max: makeMax(5) });
 
   return (
     <Main animate={{ backgroundColor: isOnScreen ? "#000" : "#f96754" }}>
       <ScrollRenderer {...makeProps(0)}>
-        <P>Hey SpaceX, my name is Ilyass Ben Hakim</P>
+        <Stack space={1}>
+          <P direction="center">Hey there! my name is</P>
+          <H1 direction="center">Ilyass Ben Hakim</H1>
+        </Stack>
       </ScrollRenderer>
       <ScrollRenderer {...makeProps(1)}>
-        <P>A clean design minded young software engineer and web designer.</P>
+        <P>
+          A <b>clean design </b>minded young software engineer and web designer.
+        </P>
       </ScrollRenderer>
       <ScrollRenderer {...makeProps(2)}>
         <P>
           I heared about your offer for React JS developper, and i would
-          integrate SpaceX team and try my best.
+          integrate your team, be a part of the journey and try my best.
         </P>
       </ScrollRenderer>
-      <ScrollRenderer {...makeProps(3.5)}>
+      <ScrollRenderer {...makeProps(3)}>
         <Stack space={1}>
           <P direction="left">So let's see</P>
           <H1>What i can do?</H1>
@@ -47,9 +52,13 @@ const P = styled.p`
   line-height: 1.2rem;
 `;
 const H1 = styled.h1`
+  margin: 0 auto;
   font-size: 3.4rem;
+  max-width: 400px;
   color: #fff;
   line-height: 3.8rem;
+  text-align: ${({ direction }: { direction: "center" | "left" | "right" }) =>
+    direction};
 `;
 const Main = styled(motion.div)`
   font-family: sans-serif;
