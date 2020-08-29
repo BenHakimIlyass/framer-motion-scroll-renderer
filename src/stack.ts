51823aa8cb2ec2bf2c4f351966c51e0d56f545c1
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2020-08-29 09:13:36 +0100

    improve design system

--- src/stack.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const VStack = styled.div`
-  ${({ top }: { top?: number }) => !!top && `margin-top:${top}rem`};
-  * + * {
-    margin-top: ${({ space }: { space: number }) => space}rem;
-  }
-`;
-export default VStack;
